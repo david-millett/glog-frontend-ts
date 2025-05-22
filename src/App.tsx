20191c987fd01bb9cca371aeaa71fcd6fa4fc80d
David <davidmillett@protonmail.com>
2025-05-22 17:14:24 +0100

    created navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 // import { useState } from 'react'
 import './App.css'
 
+// * Components
+import NavBar from './components/NavBar/NavBar'
+
+// * Pages
+
 function App() {
   
   return (
     <>
+    <NavBar />
       <h1>Hello, World!</h1>
     </>
   )
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Page 1</li>
+                <li>Page 2</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
