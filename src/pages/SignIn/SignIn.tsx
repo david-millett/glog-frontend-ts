ce4b44ea155fe21eed6767e30d7fe7129593adaa
David <davidmillett@protonmail.com>
2025-05-23 16:32:03 +0100

    created sign up form

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,3 +1,13 @@
+
+
+// Styling
+
+
+// Types
+
+
+
+// SignIn
 const SignIn = () => {
     return (
         <h1>Sign In</h1>
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,6 +1,81 @@
+import { useState } from "react"
+
+// Styling
+
+
+// Types
+
+
+
+// SignUp
 const SignUp = () => {
+
+    // Variables
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+    })
+
+    // Functions
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(formData)
+    }
+
+    // Page
     return (
-        <h1>Sign Up</h1>
+        <main>
+            <h1>Sign Up</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={formData.username}
+                        name="username"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        value={formData.email}
+                        name="email"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={formData.password}
+                        name="password"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password_confirmation">Confirm password:</label>
+                    <input
+                        type="password"
+                        id="password_confirmation"
+                        value={formData.password_confirmation}
+                        name="password_confirmation"
+                        onChange={handleChange}
+                    />
+                </div>
+                <button>Sign up</button>
+            </form>
+        </main>
     )
 }
 
